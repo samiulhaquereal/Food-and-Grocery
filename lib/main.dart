21f9664e53c7815e0e9b2639f4800f511c5da11a
B.M.Samiul Haque Real <samiulreal198@gmail.com>
2024-06-29 14:04:49 +0600

    4th

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:foodandgrocery/screen/homescreen.dart';
-import 'package:foodandgrocery/screen/splashscreen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
