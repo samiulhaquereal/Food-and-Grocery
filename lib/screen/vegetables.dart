d029602ce8a5a1457806b0d3ef31f5c17fe85e40
B.M.Samiul Haque Real <samiulreal198@gmail.com>
2024-06-29 13:56:29 +0600

    3rd

--- lib/screen/vegetables.dart
+++ lib/screen/vegetables.dart
@@ -66,116 +66,124 @@ class _VegetablesState extends State<Vegetables> {
                     ),
                   ),
                   SizedBox(height: 20,),
-                  FilterChipWidget(),
                   Expanded(
-                    child: ListView.builder(
-                      itemCount: vegetable.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        var itemname = vegetable[index];
-                        return Padding(
-                          padding: EdgeInsets.symmetric(vertical: 12),
-                          child: InkWell(
-                            onTap: () => Get.to(Veginfo(),arguments: {'image': itemname.image,'index': index}),
-                            borderRadius: BorderRadius.circular(8),
-                            child: Container(
-                              height: 150,
-                              //color: Colors.purple[100],
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(20),
-                              ),
-                              child: Row(
-                                children: [
-                                  Expanded(
-                                    child: ClipRRect(
-                                      borderRadius: BorderRadius.circular(8), // Image border
-                                      child: SizedBox.fromSize(
-                                        child: Hero(
-                                            tag: 'item_image$index',
-                                            child: Image.asset(itemname.image, fit: BoxFit.cover)),
-                                      ),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          FilterChipWidget(),
+                          ListView.builder(
+                            shrinkWrap: true,
+                            physics: ClampingScrollPhysics(),
+                            itemCount: vegetable.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              var itemname = vegetable[index];
+                              return Padding(
+                                padding: EdgeInsets.symmetric(vertical: 12),
+                                child: InkWell(
+                                  onTap: () => Get.to(Veginfo(),arguments: {'image': itemname.image,'index': index}),
+                                  borderRadius: BorderRadius.circular(8),
+                                  child: Container(
+                                    height: 150,
+                                    //color: Colors.purple[100],
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(20),
                                     ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.all(10),
-                                    child: Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                    child: Row(
                                       children: [
-                                        Text(
-                                          itemname.name,
-                                          style: TextStyle(
-                                              color: Color(0xFF2d0c57),
-                                              fontWeight: FontWeight.w600,
-                                              fontSize: 18),
-                                        ),
-                                        SizedBox(height: 10),
-                                        RichText(
-                                          text: TextSpan(
-                                            text: '${itemname.price} ',
-                                            style: TextStyle(
-                                              fontWeight: FontWeight.bold,
-                                              fontSize: 22,
-                                              color: Color(0xFF2d0c57), // Set default color for text
+                                        Expanded(
+                                          child: ClipRRect(
+                                            borderRadius: BorderRadius.circular(8), // Image border
+                                            child: SizedBox.fromSize(
+                                              child: Hero(
+                                                  tag: 'item_image$index',
+                                                  child: Image.asset(itemname.image, fit: BoxFit.cover)),
                                             ),
-                                            children: <TextSpan>[
-                                              TextSpan(
-                                                text: itemname.weight,
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding: const EdgeInsets.all(10),
+                                          child: Column(
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            children: [
+                                              Text(
+                                                itemname.name,
                                                 style: TextStyle(
-                                                  fontSize: 16,
-                                                  color: Colors.grey.shade500, // Custom color for weight
+                                                    color: Color(0xFF2d0c57),
+                                                    fontWeight: FontWeight.w600,
+                                                    fontSize: 18),
+                                              ),
+                                              SizedBox(height: 10),
+                                              RichText(
+                                                text: TextSpan(
+                                                  text: '${itemname.price} ',
+                                                  style: TextStyle(
+                                                    fontWeight: FontWeight.bold,
+                                                    fontSize: 22,
+                                                    color: Color(0xFF2d0c57), // Set default color for text
+                                                  ),
+                                                  children: <TextSpan>[
+                                                    TextSpan(
+                                                      text: itemname.weight,
+                                                      style: TextStyle(
+                                                        fontSize: 16,
+                                                        color: Colors.grey.shade500, // Custom color for weight
+                                                      ),
+                                                    ),
+                                                  ],
                                                 ),
                                               ),
+                                              Spacer(),
+                                              Row(
+                                                mainAxisAlignment:
+                                                MainAxisAlignment.spaceAround,
+                                                children: [
+                                                  InkWell(
+                                                    onTap: (){},
+                                                    child: Container(
+                                                      height: 40,
+                                                      width: 78,
+                                                      child: Icon(CupertinoIcons.heart,color: Colors.grey.shade500),
+                                                      decoration: BoxDecoration(
+                                                          color: Colors.white,
+                                                          border: Border.all(
+                                                              color: Colors.grey.shade400),
+                                                          borderRadius:
+                                                          BorderRadius.all(
+                                                              Radius.circular(10))),
+                                                    ),
+                                                  ),
+                                                  SizedBox(
+                                                    width: 15,
+                                                  ),
+                                                  InkWell(
+                                                    onTap: (){},
+                                                    child: Container(
+                                                      height: 40,
+                                                      width: 78,
+                                                      child: Icon(CupertinoIcons.cart,color: Colors.white,size: 20,),
+                                                      decoration: BoxDecoration(
+                                                          color: Color(0xFF0BCE83),
+                                                          borderRadius:
+                                                          BorderRadius.all(
+                                                              Radius.circular(10))),
+                                                    ),
+                                                  ),
+                                                ],
+                                              )
                                             ],
                                           ),
                                         ),
-                                        Spacer(),
-                                        Row(
-                                          mainAxisAlignment:
-                                          MainAxisAlignment.spaceAround,
-                                          children: [
-                                            InkWell(
-                                              onTap: (){},
-                                              child: Container(
-                                                height: 40,
-                                                width: 78,
-                                                child: Icon(CupertinoIcons.heart,color: Colors.grey.shade500),
-                                                decoration: BoxDecoration(
-                                                  color: Colors.white,
-                                                    border: Border.all(
-                                                        color: Colors.grey.shade400),
-                                                    borderRadius:
-                                                    BorderRadius.all(
-                                                        Radius.circular(10))),
-                                              ),
-                                            ),
-                                            SizedBox(
-                                              width: 15,
-                                            ),
-                                            InkWell(
-                                              onTap: (){},
-                                              child: Container(
-                                                height: 40,
-                                                width: 78,
-                                                child: Icon(CupertinoIcons.cart,color: Colors.white,size: 20,),
-                                                decoration: BoxDecoration(
-                                                    color: Color(0xFF0BCE83),
-                                                    borderRadius:
-                                                    BorderRadius.all(
-                                                        Radius.circular(10))),
-                                              ),
-                                            ),
-                                          ],
-                                        )
                                       ],
                                     ),
                                   ),
-                                ],
-                              ),
-                            ),
+                                ),
+                              );
+                            },
                           ),
-                        );
-                      },
+                        ],
+                      ),
                     ),
-                  ),
+                  )
                 ],
               ),
             ),
